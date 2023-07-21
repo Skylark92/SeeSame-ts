0566ac99ab8bbfdb6e377023f3cd736c0304aea4
Skylark92 <skylark92@live.co.kr>
2023-07-21 15:39:41 +0900

    userid  존재 여부 확인 기능 추가

---
+++ src/api/user/isHaveId.ts
@@ -0,0 +1,19 @@
+import { C_user } from 'api/core';
+import { UserData } from 'api/type/user';
+import { getDocs, query, where } from 'firebase/firestore';
+
+export default async function isHaveId(
+  id: string,
+): Promise<UserData | undefined> {
+  // db에 존재하는 아이디인지 확인
+
+  const q = query(C_user, where('userid', '==', id)); // 콜렉션에 동일한 아이디를 찾는 쿼리
+  const response = await getDocs(q); // 요청 보내고 응답 받음
+
+  let data;
+  response.forEach((doc) => {
+    data = doc.data();
+  });
+
+  return data; // 존재하는 경우 UserData, 없는 경우 undefined
+}
