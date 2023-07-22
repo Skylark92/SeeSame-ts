f29990318bd3d226937cc6d5b053ac4e58a1d3b4
Skylark92 <skylark92@live.co.kr>
2023-07-23 01:04:05 +0900

    Profile 타입 수정

--- src/api/type/user.ts
+++ src/api/type/user.ts
@@ -33,7 +33,16 @@ export type Profile = {
     | 'ISFJ'
     | 'ISTP'
     | 'ISTJ';
-  profileImage: string;
+  profileImage:
+    | 'profile-image-01'
+    | 'profile-image-02'
+    | 'profile-image-03'
+    | 'profile-image-04'
+    | 'profile-image-05'
+    | 'profile-image-06'
+    | 'profile-image-07'
+    | 'profile-image-08'
+    | 'profile-image-09';
 };
 
 export interface UserResponse extends CustomResponse {
