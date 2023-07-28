e8bbdfe1fdfbe83f7d21883066715e9fac66a3c4
Skylark92 <skylark92@live.co.kr>
2023-07-28 19:07:43 +0900

    getSurveys 쿼리 추가

--- src/api/survey/getSurveys.ts
+++ src/api/survey/getSurveys.ts
@@ -1,11 +1,12 @@
 import { C_survey } from 'api/core';
-import { getDocs } from 'firebase/firestore';
+import { getDocs, orderBy, query } from 'firebase/firestore';
 
 export default async function getSurveys(): Promise<unknown[]> {
   // 서베이 전체 목록 가져오기
 
   const surveyList: unknown[] = [];
-  const snapshot = await getDocs(C_survey);
+  const q = query(C_survey, orderBy('createdAt', 'desc'));
+  const snapshot = await getDocs(q);
 
   snapshot.forEach((doc) => {
     surveyList.push(doc.data());
