cc1d6cd8400bc00513dcd6ed528ea4e83f0ff7ff
Skylark92 <skylark92@live.co.kr>
2023-07-28 19:05:55 +0900

    user 타입 세분화

--- src/api/type/user.ts
+++ src/api/type/user.ts
@@ -14,36 +14,44 @@ export interface UserData extends User {
 
 export type Profile = {
   nickname: string;
-  gender: '남자' | '여자';
-  age: '10대' | '20대' | '30대' | '40대' | '청춘';
-  MBTI:
-    | 'ESTJ'
-    | 'ESTP'
-    | 'ESFJ'
-    | 'ESFP'
-    | 'ENTJ'
-    | 'ENTP'
-    | 'ENFJ'
-    | 'ENFP'
-    | 'INFP'
-    | 'INFJ'
-    | 'INTP'
-    | 'INTJ'
-    | 'ISFP'
-    | 'ISFJ'
-    | 'ISTP'
-    | 'ISTJ';
-  profileImage:
-    | 'profile-image-01'
-    | 'profile-image-02'
-    | 'profile-image-03'
-    | 'profile-image-04'
-    | 'profile-image-05'
-    | 'profile-image-06'
-    | 'profile-image-07'
-    | 'profile-image-08'
-    | 'profile-image-09';
+  gender: Gender;
+  age: Age;
+  MBTI: MBTI;
+  profileImage: ProfileImage;
 };
+
+export type Gender = '남자' | '여자';
+
+export type Age = '10대' | '20대' | '30대' | '40대' | '청춘';
+
+export type MBTI =
+  | 'ESTJ'
+  | 'ESTP'
+  | 'ESFJ'
+  | 'ESFP'
+  | 'ENTJ'
+  | 'ENTP'
+  | 'ENFJ'
+  | 'ENFP'
+  | 'INFP'
+  | 'INFJ'
+  | 'INTP'
+  | 'INTJ'
+  | 'ISFP'
+  | 'ISFJ'
+  | 'ISTP'
+  | 'ISTJ';
+
+export type ProfileImage =
+  | 'profile-image-01'
+  | 'profile-image-02'
+  | 'profile-image-03'
+  | 'profile-image-04'
+  | 'profile-image-05'
+  | 'profile-image-06'
+  | 'profile-image-07'
+  | 'profile-image-08'
+  | 'profile-image-09';
 
 export interface UserResponse extends CustomResponse {
   user?: UserData;
